adf12e6b0b65978254480e562e3e7d5c58909607
Ignacio Santos <ignaciosantos840@gmail.com>
2024-04-17 15:45:45 -0300

    Proyecto Final

--- javascript/catalogo.js
+++ javascript/catalogo.js
@@ -1,4 +1,3 @@
-// Array de raquetas
 let raquetas = [
     { id: 1, marca: 'PaddleMaster', tipo: 'Profesional', precio: 129.99 },
     { id: 2, marca: 'SmashTech', tipo: 'Recreativa', precio: 59.99 },
@@ -7,7 +6,6 @@ let raquetas = [
 
 let carrito = [];
 
-// Función para mostrar el catálogo de raquetas
 function mostrarCatalogo() {
     let catalogoDiv = document.getElementById('catalogoRaquetas');
     let html = '';
@@ -24,7 +22,7 @@ function mostrarCatalogo() {
 
     catalogoDiv.innerHTML = html;
 
-    // Añadir eventos a los botones de agregar al carrito
+    
     const botonesAgregarCarrito = catalogoDiv.querySelectorAll('.agregarCarrito');
     botonesAgregarCarrito.forEach((boton) => {
         boton.addEventListener('click', agregarAlCarrito);
@@ -36,34 +34,34 @@ function agregarAlCarrito(event) {
     const button = event.target;
     const raquetaId = button.closest('.raqueta').dataset.id;
 
-    // Encontrar la raqueta por su ID
+    
     const raquetaSeleccionada = raquetas.find((raqueta) => raqueta.id === parseInt(raquetaId));
 
-    // Agregar la raqueta al carrito
+    
     carrito.push(raquetaSeleccionada);
 
-    // Actualizar el carrito
+    
     actualizarCarrito();
 }
 
 // Función para actualizar el carrito
 function actualizarCarrito() {
-    // Obtener los elementos del carrito y el total
+    
     const listaCarrito = document.getElementById('listaCarrito');
     const totalCarrito = document.getElementById('totalCarrito');
 
-    // Limpiar la lista del carrito
+    
     listaCarrito.innerHTML = '';
 
-    // Calcular el total
+    
     let total = 0;
 
-    // Agregar elementos al carrito y calcular el total
+    
     carrito.forEach((raqueta, index) => {
         const li = document.createElement('li');
         li.textContent = `${raqueta.marca} - $${raqueta.precio.toFixed(2)} `;
         
-        // Botón para eliminar la raqueta del carrito
+        
         const botonEliminar = document.createElement('button');
         botonEliminar.textContent = 'Eliminar';
         botonEliminar.addEventListener('click', () => eliminarDelCarrito(index));
@@ -71,40 +69,37 @@ function actualizarCarrito() {
         li.appendChild(botonEliminar);
         listaCarrito.appendChild(li);
 
-        // Acumular el precio de la raqueta al total
+        
         total += raqueta.precio;
     });
 
-    // Actualizar el total en el HTML
+   
     totalCarrito.textContent = total.toFixed(2);
 }
 
 // Función para eliminar del carrito
 function eliminarDelCarrito(index) {
-    // Eliminar la raqueta del carrito
+   
     carrito.splice(index, 1);
     
-    // Actualizar el carrito
+    
     actualizarCarrito();
 }
 
 // Función para manejar la compra
 function comprar() {
-    // Verificar si el carrito está vacío
+    
     if (carrito.length === 0) {
-        // Si el carrito está vacío, muestra una alerta con SweetAlert
+        
         Swal.fire({
             icon: 'warning',
             title: 'Carrito vacío',
             text: 'Por favor, añade productos al carrito antes de proceder con la compra.',
             confirmButtonText: 'Aceptar'
         });
-        // No continuar con la compra si el carrito está vacío
+        
         return;
     }
-
-    // Aquí puedes agregar la lógica para continuar con la compra si el carrito no está vacío
-    // Por ejemplo, procesar la compra, redirigir al usuario a otra página, etc.
 
     Swal.fire({
         icon: 'success',
@@ -113,20 +108,17 @@ function comprar() {
         confirmButtonText: 'Aceptar'
     });
 
-    // Vaciar el carrito después de la compra
+    
     carrito.length = 0;
 
-    // Actualizar el carrito en la interfaz
+    
     actualizarCarrito();
 }
 
 
-
-
-// Añadir evento al botón de comprar
 document.getElementById('btnComprar').addEventListener('click', comprar);
 
-// Llama a la función para mostrar el catálogo al cargar la página
+
 window.onload = function() {
     mostrarCatalogo();
 };
--- javascript/reservas.js
+++ javascript/reservas.js
@@ -1,29 +1,24 @@
 async function cargarDatosJSON() {
     try {
-        // Cargar el archivo JSON
+        
         const response = await fetch('datos.json');
         
-        // Verificar si la respuesta es exitosa
         if (!response.ok) {
             throw new Error('Error al cargar datos JSON');
         }
-        
-        // Convertir la respuesta en datos JSON
+    
         const data = await response.json();
         
-        // Asignar los datos a variables globales
         reservas = data.reservas;
         catalogoRaquetas = data.catalogoRaquetas;
         
-        // Llamar a funciones para trabajar con los datos
-        cargarReservasGuardadas(); // Cambiar a cargar reservas guardadas desde datos.json
-        mostrarCatalogo(); // Mostrar el catálogo de raquetas
+        cargarReservasGuardadas(); 
+        mostrarCatalogo(); 
     } catch (error) {
         console.error('Error al cargar datos JSON:', error);
     }
 }
 
-// Llamar a la función para cargar los datos JSON al inicio
 cargarDatosJSON();
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -44,7 +39,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     try {
-        // Asignar eventos para las celdas de la primera tabla
+        
         document.querySelectorAll('#turnos .horario').forEach(function (horario) {
             horario.addEventListener('click', toggleSeleccion);
         });
@@ -70,14 +65,14 @@ document.addEventListener('DOMContentLoaded', function () {
 // Función asincrónica para cargar reservas guardadas desde el archivo JSON
 async function cargarReservasGuardadas() {
     try {
-        // Realizar una solicitud para obtener datos del archivo JSON
+        
         const response = await fetch('datos.json');
         const data = await response.json();
 
-        // Verificar si hay datos de reservas en la respuesta
+       
         const reservas = data.reservas || {};
 
-        // Iterar sobre los datos de reservas y actualizar las celdas en la página
+        
         for (let dia in reservas) {
             for (let hora in reservas[dia]) {
                 let celda = document.querySelector(`.horario[data-dia="${dia}"][data-hora="${hora}"]`);
@@ -95,10 +90,10 @@ async function cargarReservasGuardadas() {
 
 async function reservarTurno() {
     try {
-        // Obtener la celda seleccionada
+        
         let selectedCell = document.querySelector('.selected');
 
-        // Verificar que se haya seleccionado una celda
+        
         if (!selectedCell) {
             Swal.fire({
                 icon: 'warning',
@@ -108,11 +103,11 @@ async function reservarTurno() {
             return;
         }
 
-        // Obtener los atributos data de la celda seleccionada
+        
         const dia = selectedCell.getAttribute('data-dia');
         const hora = selectedCell.getAttribute('data-hora');
 
-        // Solicitar el nombre del usuario
+        
         Swal.fire({
             title: 'Ingrese su nombre',
             input: 'text',
@@ -139,19 +134,19 @@ async function reservarTurno() {
                         let telefono = result.value;
 
                         if (nombre && telefono) {
-                            // Actualizar la celda con el nombre
+                            
                             selectedCell.textContent = nombre;
                             selectedCell.classList.add('reservado');
                             selectedCell.classList.remove('selected');
 
-                            // Mostrar mensaje de confirmación
+                            
                             Swal.fire({
                                 icon: 'success',
                                 title: 'Reserva exitosa',
                                 text: `Turno reservado para ${nombre}.`,
                             });
 
-                            // Guardar la reserva en localStorage
+                            
                             await guardarReservaEnLocalStorage(dia, hora, nombre, telefono);
                         } else {
                             Swal.fire({
@@ -173,17 +168,15 @@ async function reservarTurno() {
 
 // Guardar reserva en localStorage
 async function guardarReservaEnLocalStorage(dia, hora, nombre, telefono) {
-    // Obtener reservas existentes de localStorage
     let reservasGuardadas = localStorage.getItem('reservas');
     let reservas = reservasGuardadas ? JSON.parse(reservasGuardadas) : {};
 
-    // Crear o actualizar la reserva para el día y hora específicos
+
     if (!reservas[dia]) {
         reservas[dia] = {};
     }
     reservas[dia][hora] = { nombre, telefono };
 
-    // Almacenar las reservas actualizadas en localStorage
     localStorage.setItem('reservas', JSON.stringify(reservas));
 
     console.log('Reserva guardada con éxito en localStorage.');
@@ -191,14 +184,11 @@ async function guardarReservaEnLocalStorage(dia, hora, nombre, telefono) {
 
 // Cargar reservas guardadas desde localStorage
 function cargarReservasGuardadas() {
-    // Obtener reservas de localStorage
     const reservasGuardadas = localStorage.getItem('reservas');
 
-    // Verificar si hay datos de reservas guardados en localStorage
     if (reservasGuardadas) {
         const reservas = JSON.parse(reservasGuardadas);
 
-        // Iterar sobre los datos de reservas y actualizar las celdas en la página
         for (let dia in reservas) {
             for (let hora in reservas[dia]) {
                 let celda = document.querySelector(`.horario[data-dia="${dia}"][data-hora="${hora}"]`);
@@ -214,31 +204,24 @@ function cargarReservasGuardadas() {
 
 // Eliminar reserva en localStorage
 function eliminarReservaEnLocalStorage(dia, hora) {
-    // Obtener reservas existentes de localStorage
     let reservasGuardadas = localStorage.getItem('reservas');
     let reservas = reservasGuardadas ? JSON.parse(reservasGuardadas) : {};
 
-    // Verificar si hay reservas para el día y hora especificados
     if (reservas[dia] && reservas[dia][hora]) {
-        // Eliminar la reserva específica
+ 
         delete reservas[dia][hora];
 
-        // Si ya no hay reservas para el día, eliminar el objeto del día
         if (Object.keys(reservas[dia]).length === 0) {
             delete reservas[dia];
         }
 
-        // Actualizar localStorage con las reservas modificadas
         localStorage.setItem('reservas', JSON.stringify(reservas));
     }
 }
 
-//eliminarReservaEnLocalStorage('Lunes','19:00')
-
-
 async function cargarDatos() {
     try {
-        // Cargar datos de datos.json
+       
         const response = await fetch('datos.json');
         if (!response.ok) {
             throw new Error('Error al cargar datos JSON');
@@ -246,14 +229,12 @@ async function cargarDatos() {
         const data = await response.json();
         const reservasJSON = data.reservas || {};
 
-        // Cargar datos de localStorage
+        
         const reservasLocalStorageString = localStorage.getItem('reservas');
         const reservasLocalStorage = reservasLocalStorageString ? JSON.parse(reservasLocalStorageString) : {};
 
-        // Combinar las reservas de JSON y localStorage con prioridad para las de localStorage
         const reservasCombinadas = { ...reservasJSON };
 
-        // Sobreescribir reservas de JSON con las de localStorage para priorizar las más recientes
         for (const dia in reservasLocalStorage) {
             if (reservasLocalStorage.hasOwnProperty(dia)) {
                 if (!reservasCombinadas[dia]) {
@@ -265,7 +246,6 @@ async function cargarDatos() {
             }
         }
 
-        // Iterar sobre las reservas combinadas y actualizar la página
         for (const dia in reservasCombinadas) {
             for (const hora in reservasCombinadas[dia]) {
                 const reserva = reservasCombinadas[dia][hora];
@@ -281,5 +261,5 @@ async function cargarDatos() {
     }
 }
 
-// Llama a la función para cargar los datos al inicio
+
 cargarDatos();
